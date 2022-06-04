08c66c3b7cd44ed29ee8f6ee744d7c2b610bef87
Hasan <Hasanaldhahi3@gmail.com>
2022-06-04 18:50:38 +0300

    assignment finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+  const inputText = document.getElementById("new-task-description")
+
+  const submitBtn = document.getElementById("create");
+
+
+  const tasks = document.getElementById("tasks");
+
+
+  const id = 0
+
+
+  submitBtn.addEventListener('click', (e) => {
+
+    e.preventDefault();
+
+
+
+    tasks.innerHTML += `<li id="${id}" >${inputText.value} <button name = "hasan" class ="hasan ${id}">X</button></li> `;
+    inputText.value = "";
+  })
+
+
+
+  document.getElementsByName(".hasan").forEach(element => {
+    element.addEventListener("click", () => {
+
+      console.log("clicked")
+
+      const item = document.getElementById(`${element.className.split(" ")[1]}`)
+      console.log(item)
+      item.style.visibility = "hidden"
+    }
+    )
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
